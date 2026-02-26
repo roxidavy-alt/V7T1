798d2528c03943564702652b150d45c8c169a357
Gena <genrichkudes72@gmail.com>
2026-02-26 13:11:13 +0200

    1

--- src/main/Book.java
+++ src/main/Book.java
@@ -1,4 +1,24 @@
 package main;
-public class Book {
-    
+
+public abstract class Book {
+    protected String title;
+    protected String author;
+    protected int pages;
+    protected int copies;
+
+    public void displayInfo(){
+        System.out.println("Nimi: " + title + ", Kirjailija: " + author + ", Sivut: " + pages + ", Määrä: " + copies);
+    }
+
+    public abstract String categorize();
+
+    public String getTitle(){
+        return title;
+    }
+    public Book(String title, String author, int pages, int copies){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.copies = copies;
+    }
 }
