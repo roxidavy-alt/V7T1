56d6064a0a87da4435cc7d13d4e2427d05022c9a
Gena <genrichkudes72@gmail.com>
2026-02-26 13:28:19 +0200

    2

--- src/main/FictionBook.java
+++ src/main/FictionBook.java
@@ -1,5 +1,12 @@
 package main;
 
-public class FictionBook {
-    
+public class FictionBook extends Book{
+    public FictionBook(String title, String author, int pages, int copies){
+        super(title, author, pages, copies);
+    }
+
+    @Override
+    public String categorize() {
+        return "Fiktiokirja";
+    }
 }
--- src/main/NonFictionBook.java
+++ src/main/NonFictionBook.java
@@ -1,5 +1,12 @@
 package main;
 
-public class NonFictionBook {
-    
+public class NonFictionBook extends Book{
+    public NonFictionBook(String title, String author, int pages, int copies){
+        super(title, author, pages, copies);
+    }
+
+    @Override
+    public String categorize() {
+        return "Tietokirja";
+    }
 }
